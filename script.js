4147ffc5e462c9ffaaf1ec9e0d6f5b83f11169e0
Demetrio <demetrioalmeida@gmail.com>
2020-05-02 13:56:59 -0400

    Homework

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
 // Initial array of citiesArray
-var citiesArray = [];
+var citiesArray = JSON.parse(localStorage.getItem("cityName")) || [];
+
+// setting a varible for my API key
+var apiKey = "f9476d710a0f07f0c50dc74e1e030424";
+
+// Function that display creatte new buttons on search
+function renderButtons() {
+    $("#buttons-view").empty();
+    $("#cityInput").val("");
+
+    // Looping through the array of citiesArray
+    for ( i = 0; i < citiesArray.length; i++) {
+    // The dynamicaly generating buttons for each city in the array
+        var cityBtn = $("<button>");
+        cityBtn.addClass("list-group-item city-btn");
+        cityBtn.attr("data-name", citiesArray[i]);
+        cityBtn.text(citiesArray[i]);
+        $("#buttons-view").append(cityBtn);
+}
+}
+
+//This function handles events where one button is clicked
+$("#search-button").on("click", function(event) {
+
+// event.preventDefault() prevents the form from trying to submit itself.
+    event.preventDefault();
+
+    var cityName = $("#cityInput").val().trim();
+
+    if (cityName) {
+        // Save searched city to localStorage
+        citiesArray.push(cityName);
+        localStorage.setItem("cityName", JSON.stringify(citiesArray));
+        renderButtons();
+    }
+})
 
 // Function for dumping the JSON content for each button into the div
 function displayWeatherInfo() {
-    alert("it works");
 
     var city = $(this).attr("data-name");
     console.log(city);
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=f9476d710a0f07f0c50dc74e1e030424";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
     $.ajax({
         url: queryURL,
@@ -17,49 +51,7 @@ function displayWeatherInfo() {
     });
 }
 
-// Function that display creatte new buttons on search
-function renderButtons() {
-    $("#buttons-view").empty();
-    $("#cityInput").val("");
-
-// It gets values saved from localStorage
-    // localStorage.getItem(cityName);
-    var recentlySearchOnLocalStorage = localStorage.getItem("cityName");
-    console.log(recentlySearchOnLocalStorage);
-    citiesArray.push(recentlySearchOnLocalStorage);
-
-// Looping through the array of citiesArray
-    for ( i = 0; i < citiesArray.length; i++) {
-        
-// The dynamicaly generating buttons for each city in the array
-        var recentlySearchList = $("<li>");
-        recentlySearchList.addClass("list-group-item searchedList");
-        recentlySearchList.attr("data-name", citiesArray[i]);
-        recentlySearchList.text(citiesArray[i]);
-// Adding buttons to the HTML document
-        $("#buttons-view").append(recentlySearchList);
-    }
-}
-
-//This function handles events where one button is clicked
-$("#search-button").on("click", function(event) {
-// event.preventDefault() prevents the form from trying to submit itself.
-    event.preventDefault();
-
-    var cityName = $("#cityInput").val().trim();
-    citiesArray.push(cityName);
-
-// Save searched city to localStorage
-    localStorage.setItem("cityName", cityName);
-    if (cityName === "") {
-        alert("The city name value can not be empty.")
-    } else {
-    renderButtons();
-    }
-
-// Function for displaying the movie info
-    $(document).on("click", ".searchedList", displayWeatherInfo);
-})
-
+// Function to display the weather info
+$(document).on("click", ".city-btn", displayWeatherInfo);
 // Calling the renderButtons function at least once to display the inicial list of citiesArray
 renderButtons();
